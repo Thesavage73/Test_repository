feedbba07ff501a27f48993f36e46b9097a51f23
csavage <cs845221@ohio.edu>
2023-02-07 21:56:24 -0500

    test run

---
+++ default_program.cc
@@ -0,0 +1,22 @@
+/**
+ *   @file: invoice.cc
+ * @author: Enter your name
+ *   @date: Enter the date
+ *  @brief: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+///Constants and function prototypes
+
+int main() {
+    cout << "hello world!" << endl;
+
+    
+    
+    
+   return 0;
+} /// main
